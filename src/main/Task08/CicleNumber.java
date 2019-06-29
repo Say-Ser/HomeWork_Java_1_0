d84c8cdf31206c4958592d3e2fb1df7dfc959883
Say-Ser <Sayapin94@yandex.ru>
2019-06-29 11:23:05 +0300

    Задача №8 Вывести с помощью цикла числа от 1 до 50.

---
+++ src/main/Task08/CicleNumber.java
@@ -0,0 +1,9 @@
+package main.Task08;
+
+public class CicleNumber {
+    public static void main(String[] args) {
+        for (int i=1;i<=50;i++){
+            System.out.print(i+" ");
+        }
+    }
+}
