1998337451539fe734302b7b64f41d8ac416a8c8
Say-Ser <Sayapin94@yandex.ru>
2019-06-29 11:04:20 +0300

    Задача №6

---
+++ src/main/Task06/Calc.java
@@ -0,0 +1,20 @@
+package main.Task06;
+
+import java.util.Scanner;
+
+public class Calc {
+    public static void main(String[] args) {
+        int x,y,z;
+        float avg;
+        Scanner in= new Scanner(System.in);
+        System.out.println("Введите целые x, y, z : ");
+        x=in.nextInt();
+        y=in.nextInt();
+        z=in.nextInt();
+        avg= (float)((x+y+z)/3.0);
+        System.out.println("Среднее арифметическое x, y, z: " +avg) ;
+        if (avg/2>3) {
+            System.out.println("Программа выполнена корректно");
+        }
+    }
+}
