02cc08a64f5f0396a078efec4dc3178fc93f02a2
Say-Ser <Sayapin94@yandex.ru>
2019-07-01 00:06:41 +0300

    Задачи №15 Сортировка методом пузырька (Не работает)

---
+++ src/main/Task15/SortBuble.java
@@ -0,0 +1,37 @@
+package main.Task15;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class SortBuble {
+    public static void main(String[] args) {
+        int size;
+        Random random=new Random();
+        Scanner in= new Scanner(System.in);
+        System.out.println("Введите размерность массива: ");
+        size= in.nextInt();
+        int mass[]=new int[size];
+        for (int i=0;i<size;i++)
+        {
+            mass[i] = random.nextInt(20);
+        }
+        System.out.println("Ваш массив:");
+        for (int i=0;i<size;i++) {
+            System.out.print(mass[i]+" ");
+        }
+        for(int i = 0 ; i < size-1 ; i++){
+            for(int j = 0 ; j <size-1 ; j++){
+                if( mass[i] > mass[j+1] ){
+                int tmp = mass[i];
+                mass[i] = mass[j+1];
+                mass[j+1] = tmp;
+            }
+        }
+    }
+        System.out.println("Ваш массив отсортирован:");
+        for (int i:mass
+        ) {
+            System.out.print(mass[i]+" ");
+        }
+    }
+}
